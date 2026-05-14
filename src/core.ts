93341dd0af5e217d7baec81ab6704f0260478f15
William Inkari Huaman Lazo <wiinhula.2023@gmail.com>
2026-05-14 11:40:03 -0300

    Update core.ts

--- src/core.ts
+++ src/core.ts
@@ -1,19 +1,46 @@
-class item {
-    constructor(public tittle: string) { }
+class Item {
+  constructor(public item: string) { }
 }
 class TodoList {
-    private items: Item[] = [];
-    private filePath: string;
-    constructor(filePath: string) {
-        this.filePath = filePath;
-    }
-    addItem(item: Item) {
-        this.items.push(item);
-    }
-    removeItems(index: number) {
-        this.items.splice(index, 1);
-    }
-    getItems(): Item[] {
-        return [...this.items];
-    }
+  private items: Item[] = [];
+  private filePath: string;
+
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+  
+  private async saveListToDisk(){
+    const file = Bun.file(this.filePath)
+    const data = JSON.stringify(this.items)
+    await file.write(data)
+  }
+/**
+ * Adiciona um novo item
+ */
+  
+private async readListFromDisk(){
+    const file = Bun.file(this.filePath)
+    const data = await file.json()
+    this.items = data.map((v:any)=>{
+        return new Item(v.tittle)
+    })
 }
+  async addItem(item: Item) {
+    this.items.push(item)
+    await this.saveListToDisk()
+  }
+/**
+ *Remove um item especifico 
+ */
+async removeItem(index: number) {
+    this.items.splice(index, 1);
+    await this.saveListToDisk()
+  }
+/**
+ * Returna uma copia da lista de itens
+ */
+  async getItems() {
+    return Array.from(this.items)
+  }
+}
+const lista = new TodoList('arquivo.json')
