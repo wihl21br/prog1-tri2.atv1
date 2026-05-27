720ea37c48d9796916cbb10eaef1d38effd3fb10
wihl21br <wiinhula.2023@gmail.com>
2026-05-27 15:00:29 -0300

    atualização do dia 27_5_2026

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,19 @@
 import TodoList, { Item } from "./core"
-const todoList = new TodoList('todolist.json')
 
+const todoList = new TodoList('todolist.json')
 const params = process.argv
 const command = params[2]
+
+
+if (command === "list") {
+    const items = await todoList.getItems()
+    console.log("items atualmente na lista")
+
+    items.forEach((item, index) => {
+        console.log(`${index} : ${item.title}`)
+    })
+    process.exit(0)
+}
 
 if (command === "add") {
     const value = params[3]
@@ -13,7 +24,7 @@ if (command === "add") {
 
     
     try{
-    await todoList.addItem(new Item(value))}
+        await todoList.addItem(new Item(value))}
     catch(error){
         console.error('Erro ao adicionar um item',error)
         process.exit(1)
@@ -22,4 +33,19 @@ if (command === "add") {
     process.exit(0)
 }
 
+if (command === "remove") {
+    const indexSTR = params[3]
+    if (!indexSTR) {
+        console.error("Valor negado pois e nulo ou vazio")
+        process.exit(1)
+    } 
+    const index = parseInt(indexSTR)
+    if (isNaN(index)) {
+        console.error("indice precisa ser um numero: ", indexSTR)
+        process.exit(1)
+    }
+    await todoList.removeItem(index)
+    console.log("item removido com sucesso: ", index)
+    process.exit(0)
+}
 console.log("não entrou em if nenhum")
--- src/core.ts
+++ src/core.ts
@@ -1,17 +1,19 @@
+///
 class Item {
   constructor(public title: string) { }
 }
 class TodoList {
-  private items: Item[] = [];
+  private items: Promise<Item[]>
   private filePath: string;
 
   constructor(filePath: string) {
     this.filePath = filePath;
+    this.items = this.readListFromDisk()
   }
   
   private async saveListToDisk(){
     const file = Bun.file(this.filePath)
-    const data = JSON.stringify(this.items)
+    const data = JSON.stringify(await this.items)
     await file.write(data)
   }
 /**
@@ -21,32 +23,42 @@ class TodoList {
 private async readListFromDisk(){
     const file = Bun.file(this.filePath)
     const data = await file.json()
-    this.items = data.map((v:any)=>{
+    const items: Item[]  = data.map((v:any)=>{
         return new Item(v.tittle)
     })
+    return items;
 }
   async addItem(item: Item) {
+    const items = await this.items
     if(!item)
       throw 'Item nao pode ser nulo ou indefinido'
     if(!item.title.trim()|| !item.title)
       throw 'Item title nao pode ser nulo ou indefinido'
-    this.items.push(item)
+    items.push(item)
     await this.saveListToDisk()
   }
 /**
  *Remove um item especifico 
  */
 async removeItem(index: number) {
-    this.items.splice(index, 1);
+    const items = await this.items
+    items.splice(index, 1);
     await this.saveListToDisk()
   }
 /**
  * Returna uma copia da lista de itens
  */
+async update(index:number, newItem: Item) {
+  const items = await this.items
+  items[index] = newItem
+  await this.saveListToDisk()
+}
   async getItems() {
-    return Array.from(this.items)
+    const items = await this.items
+    return Array.from(items)
   }
 }
+
 const lista = new TodoList('arquivo.json')
 
 export default TodoList;
