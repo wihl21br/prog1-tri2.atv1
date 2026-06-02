edbf1d26199a5661d72af3a34266272f55a60a33
wihl21br <wiinhula.2023@gmail.com>
2026-06-02 13:36:22 -0300

    Atualização

--- src/cli.ts
+++ src/cli.ts
@@ -4,13 +4,12 @@ const todoList = new TodoList('todolist.json')
 const params = process.argv
 const command = params[2]
 
-
 if (command === "list") {
     const items = await todoList.getItems()
     console.log("items atualmente na lista")
 
     items.forEach((item, index) => {
-        console.log(`${index} : ${item.title}`)
+        console.log(`item ${index+1} no index ${index} : ${item.title}`)
     })
     process.exit(0)
 }
@@ -50,9 +49,35 @@ if (command === "remove") {
 }
 if (command === "update") {
     const indexSTR = params[3]
-    const newValue = params[4]
-    await todoList.update(parseInt(indexSTR), new Item(newValue))
-    console.log("item atualizado com sucesso: ", indexSTR, newValue)
+    const valor_novo = params[4]
+    if (!indexSTR || !valor_novo) {
+        console.error("Valor negado pois e nulo ou vazio")
+        process.exit(1)
+    }
+    const index = parseInt(indexSTR)
+    if (isNaN(index)) {
+        console.error("indice precisa ser um numero: ", indexSTR)
+        process.exit(1)
+    }
+    if(index < 0){
+        console.error("indice precisa ser um numero positivo e que seja conduz o numero da lista: ", index)
+        process.exit(1)
+    }
+    if(!valor_novo){
+        console.error("novo valor nao pode ser vazio ou nulo: ", valor_novo)
+        process.exit(1)
+    }
+    await todoList.updateItem(index, new Item(valor_novo))
+    console.log("item no index ",index," atualizado com sucesso para: ", valor_novo)
     process.exit(0)
 }
-console.log("não entrou em if nenhum")
+
+if (command)
+    console.log(`Comando não reconhecido: ${command}`)
+
+console.log(`Comandos disponíveis:
+- add <item>: Adiciona um item à lista
+- remove <index>: Remove um item da lista por indice
+- list: Mostra os itens atuais da lista
+- update <index do item que sera atualizado> <novo valor desse index>: Atualiza um item da lista pelo indice
+`)
--- src/core.ts
+++ src/core.ts
@@ -1,72 +1,56 @@
-///
-/**
- * @todo
- * known issues:
- * - getItems needs to await loadListFromDisk()
- */
-
-
 class Item {
-  constructor(public title: string) { }
+    constructor(public title: string) { }
 }
+
 class TodoList {
-  private items: Promise<Item[]>
-  private filePath: string;
+    private items: Promise<Item[]>;
+    private filePath: string;
 
-  constructor(filePath: string) {
-    this.filePath = filePath;
-    this.items = this.readListFromDisk()
-  }
-  
-  private async saveListToDisk(){
-    const file = Bun.file(this.filePath)
-    const data = JSON.stringify(await this.items)
-    await file.write(data)
-  }
-/**
- * Adiciona um novo item
- */
-  
-private async readListFromDisk(){
-    const file = Bun.file(this.filePath)
-    const data = await file.json()
-    const items: Item[]  = data.map((v:any)=>{
-        return new Item(v.tittle)
-    })
-    return items;
-}
-  async addItem(item: Item) {
-    const items = await this.items
-    if(!item)
-      throw 'Item nao pode ser nulo ou indefinido'
-    if(!item.title.trim()|| !item.title)
-      throw 'Item title nao pode ser nulo ou indefinido'
-    items.push(item)
-    await this.saveListToDisk()
-  }
-/**
- *Remove um item especifico 
- */
-async removeItem(index: number) {
-    const items = await this.items
-    items.splice(index, 1);
-    await this.saveListToDisk()
-  }
-/**
- * Returna uma copia da lista de itens
- */
-async update(index:number, newItem: Item) {
-  const items = await this.items
-  items[index] = newItem
-  await this.saveListToDisk()
-}
-  async getItems() {
-    const items = await this.items
-    return Array.from(items)
-  }
-}
+    constructor(filePath: string) {
+        this.filePath = filePath;
+        this.items = this.loadListFromDisk();
+    }
 
-const lista = new TodoList('arquivo.json')
+    private async saveListToDisk() {
+        const file = Bun.file(this.filePath);
+        const data = JSON.stringify(await this.items);
+        await file.write(data)
+    }
 
+    private async loadListFromDisk() {
+        const file = Bun.file(this.filePath);
+        const data = await file.json() as Item[];
+        const items = data.map((v: any) => new Item(v.title));
+        return items;
+    }
+
+    async addItem(item: Item) {
+        const items = await this.items
+        if (!item)
+            throw "Item Inválido";
+        if (!item.title.trim())
+            throw "Item deve conter um título"
+        items.push(item);
+        await this.saveListToDisk();
+    }
+
+    async removeItem(index: number) {
+        const items = await this.items;
+        items.splice(index, 1);
+        await this.saveListToDisk();
+    }
+
+    async updateItem(index: number, item: Item) {
+        const items = await this.items
+        items[index] = item
+        await this.saveListToDisk()
+    }
+
+    async getItems() {
+        const items = await this.items;
+        return Array.from(items);
+    }
+
+}
 export default TodoList;
-export {Item, TodoList}
+export { TodoList, Item }
